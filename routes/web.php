ad1b9c6f13c05aa9a0e837169a01a8475ff8d7c9
Thallium <crazzzyvio@gmail.com>
2025-03-02 14:57:32 +0000

    other updates

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [UserController::class,'index'])->name('user.show.home');
+
+Route::get('/login', [AuthController::class,'loginPage'])->name('auth.show.login');
+Route::get('/registration', [AuthController::class,'registrationPage'])->name('auth.show.registration');
