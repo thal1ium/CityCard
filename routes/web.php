c3a2ec7e0f942761c65b6b3e3854b60ac63183ff
Thallium <crazzzyvio@gmail.com>
2025-03-05 13:25:57 +0000

    Add routes for user in web.php and add guards, providers in auth.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\UserAuthController;
 
-Route::get('/', [UserController::class,'index'])->name('user.show.home');
+Route::get('/', [UserController::class, 'index'])->name('user.show.home');
 
-Route::get('/login', [AuthController::class,'loginPage'])->name('auth.show.login');
-Route::get('/registration', [AuthController::class,'registrationPage'])->name('auth.show.registration');
+// User Auth
+Route::get('/login', [UserAuthController::class, 'showLoginPage'])->name('user.show.login');
+Route::get('/register', [UserAuthController::class, 'showRegisterPage'])->name('user.show.register');
+Route::post('/login', [UserAuthController::class, 'login'])->name('user.login');
+Route::post('/register', [UserAuthController::class, 'register'])->name('user.register');
+Route::post('/logout', [UserAuthController::class, 'logout'])->name('user.logout');
