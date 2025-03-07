33b735cc844458ebd00feda873b3ae2b87a10960
Thallium <crazzzyvio@gmail.com>
2025-03-07 14:04:22 +0000

    Add middlewares

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,25 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Auth\UserAuthController;
 
-Route::get('/', [UserController::class, 'index'])->name('user.show.home');
+Route::get("/", [UserController::class, "index"])->name('');
 
-// User 
+// User
 Route::get('/login', [UserAuthController::class, 'showLoginPage'])->name('user.show.login');
 Route::get('/register', [UserAuthController::class, 'showRegisterPage'])->name('user.show.register');
 Route::post('/login', [UserAuthController::class, 'login'])->name('user.login');
 Route::post('/register', [UserAuthController::class, 'register'])->name('user.register');
 Route::post('/logout', [UserAuthController::class, 'logout'])->name('user.logout');
 
+Route::middleware('auth:web')->group(function () {
+  Route::get('/home', [UserController::class, 'index'])->name('user.show.home');
+});
+
+
 // Admin
 Route::get('/admin/login', [AdminAuthController::class, 'loginPage'])->name('admin.show.login');
 Route::post('/admin/login', [AdminAuthController::class, 'login'])->name('admin.login');
+Route::post('/admin/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+
 Route::middleware('auth:admin')->group(function () {
-  Route::get('/dashboard', [AdminController::class,'index'])->name('admin.dashboard');
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
 });
